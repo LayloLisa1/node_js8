770716342e476ffba7e2b0845d5828faed941794
LayloLisa1 <148028105+LayloLisa1@users.noreply.github.com>
2024-05-04 00:14:32 +0500

    Add files via upload

---
+++ users.controller.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, '../database/users.json');
+
+const getUsers = (req, res) => {
+};
+
+const getUserById = (req, res) => {
+};
+
+const createUser = (req, res) => {
+};
+
+const updateUser = (req, res) => {
+};
+
+const deleteUser = (req, res) => {
+};
+
+module.exports = { getUsers, getUserById, createUser, updateUser, deleteUser };
